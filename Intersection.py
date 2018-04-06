c40a1d3db8b7bf9991534be8ca2bf7a8296144c7
mthurmaier <mthurmai@gmail.com>
2018-04-06 15:40:29 -0700

    Changed objects to persist in Redis hash table instead of GlobalIntersection[] memory array. Now it will work in a cluster.  More changes needed: A) add URIs to start/stop Intersections; B) start 'Running' Intersections on startup; C) As part of (A), recognize when an Intersection is stopped on a different container than the one running the timer thread (via Redis data watching)

--- Intersection.py
+++ Intersection.py
@@ -1,57 +1,192 @@
-from flask import Flask, request, jsonify
+from flask import Flask, Response, jsonify
 from threading import Thread
-import time
+import time, pickle, json
+from redis import Redis, RedisError
 
-GlobalIntersections = []
+DB_NAME = 'Intersection'
+
+GlobalTimerThreads = {}
+
+redis = Redis(host="localhost", db=0)
+# print("redis=",redis)
+# newID = redis.incr("foo")
+# print("redis=",redis, "newID=",newID)
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
-@app.route("/index")
-def getIntersections():
-    html = '<h3>List of {x} Intersections</h3>'
-    if len(GlobalIntersections) == 0:
-        html += '<p>No Intersections</p>'
+def getAllIntersections():
+    try:
+        x = redis.hkeys(DB_NAME)
+    except RedisError:
+        js = { 'error': 'database error retrieving list of intersections'}
+        js = json.dumps(js)
+        resp = Response(js, status=500, mimetype='application/json')
+        return resp
+    if len(x) == 0:
+        x = { 'note': 'Currently no intersections'}
+        return jsonify(x)
     else:
-        idx = 0
-        for signal in GlobalIntersections:
-            html += '<p>id- '
-            html += idx.__str__()
-            html += '. '
-            html += signal.getStatus().__str__()
-            html += '</p>'
-            idx += 1
+        jsarray = []
+        for idx in x:
+            res, ints = read_from_redis(idx)  # just assume success
+            js = json.dumps(ints.__dict__)
+            jsarray.append(js)      
+        return jsonify(jsarray)
   
-    return html.format(x=len(GlobalIntersections))
+@app.route("/<int:idx>", methods=['GET'])
+def getIntersection(idx):
+    # retrieve the intersection
+    print("cyclLights for ",idx)
+    res, ints = read_from_redis(idx)
+    if res != 0:
+        if res == 2: # db error
+            js = { 'error': 'database error retrieving requested record', 'idx': idx}
+            js = json.dumps(js)
+            resp = Response(js, status=500, mimetype='application/json')
+            return resp
+        elif res == 1:  # it went away
+            js = { 'error': 'intersection not found', 'idx': idx}
+            js = json.dumps(js)
+            resp = Response(js, status=404, mimetype='application/json')
+            return resp
+    js = json.dumps(ints.__dict__)
+    resp = Response(js, status=200, mimetype='application/json')
+    return resp
+    
+    return jsonify(ints)
 
 @app.route("/", methods=['POST'])
 def createIntersection():
-    GlobalIntersections.append(Intersection())
-    data = "{ 'id': "+(len(GlobalIntersections)-1).__str__()+"}"
+    print("in creatIntersection()")
+    try:
+        # create a new ID
+        nextID = redis.incr("intersectionID")
+        myID = nextID - 1
+        data = "{ 'id': "+myID.__str__()+"}"
+    except RedisError:
+        data = "{ 'error': <i>connot connect to Redis, counter disabled</i>}"
+        return jsonify(data)
+    # instantiate a new Intersection object, associate it with the ID
+    # then store it in the database
+    new_int = Intersection(myID)
+    if write_to_redis(new_int) != 0:
+        data = "{ 'error': <i>reddis hset error on newIntj</i>}"
     return jsonify(data)
 
+def write_to_redis(intersection):
+    res = 0  # assume success
+    int_pkld = pickle.dumps(intersection)
+    try:
+        redis.hset(DB_NAME,intersection.myID,int_pkld)
+    except RedisError:
+        print("redis error writing intersection object ",intersection.myID," to hash");
+        res = 1
+    return res
+
+def read_from_redis(idx,):
+    return_num = 0 # assume success
+    return_obj = "" # essentiall null string
+    try:
+        int_pkld = redis.hget(DB_NAME,idx)
+    except RedisError:
+        return 2, "Redis Exception reading idx"
+    
+    if(int_pkld == "None"): # such a thing does not exist
+        return_num = 1
+        return_obj = int_pkld
+    else:
+        intersect = pickle.loads(int_pkld)
+        return_obj = intersect
+    return return_num, return_obj
+        
+    
 class Intersection:
-    GREENTIME = 27  # green runs for 27 seconds
+    GREENTIME = 7  # green runs for 27 seconds
     YELLOWTIME = 3  # yellow runs for 3 seconds
     REDTIME = 0  # essentially red waits forever - until the other directions complete
     LightTimes = { 'Green': GREENTIME, 'Yellow': YELLOWTIME, 'Red': REDTIME}
-    LightNext = {'Green:': 'Yellow', 'Yellow': 'Red', 'Red': 'Green' }
+    DirectionNext = {'North': 'South', 'South': 'East', 'East': 'West', 'West': 'North' }
     
 
-    def __init__(self):
+    def __init__(self,idx):
+        self.myID = idx
         self.lightSet = {  # initial states for which light-set and which light color is active
             'North': { 'activeLight': 'Green', 'isActive': True},
             'South': { 'activeLight': 'Red', 'isActive': False},
             'East': {'activeLight': 'Red', 'isActive': False},
             'West': {'activeLight': 'Red', 'isActive': False} 
         }
-        self.curActive = 'North'
-        self.thread = Thread(target = Intersection.cycleLights, args=[self])
+        self.curActiveLightSet = 'North'
+        self.curState = 'Running'
+        self.curActiveLightTimeLeft = self.GREENTIME
+        GlobalTimerThreads[idx] = Thread(target = Intersection.timerFire, args=[idx])
         # self.timer = threading.Timer(Intersection.GREENTIME,Intersection.cycleLights, args=[self])
         # self.timer.start()
-        self.thread.start()
-
-    def cycleLights(self):
+        GlobalTimerThreads[idx].start()
+    
+    @staticmethod
+    def timerFire(idx):
+        print("timerFire for ",idx)
+        while True:
+            time.sleep(1)
+            if Intersection.cycleLights(idx) == 1:
+                return  # we're done
+    
+    @staticmethod
+    def cycleLights(idx):
+        # retrieve the intersection
+        print("cyclLights for ",idx)
+        res, ints = read_from_redis(idx)
+        if res != 0:
+            if res == 2: # db error
+                print("could not retrieve intersection:",idx," or unpickle it")
+                # should return 5xx error
+                js = { 'error': 'database error retrieving requested record', 'idx': idx}
+                js = json.dumps(js)
+                resp = Response(js, status=500, mimetype='application/json')
+                return resp
+            elif res == 1:  # it went away
+                print("could not find intersection number ",idx)
+                # should return 404
+                js = { 'error': 'intersection not found', 'idx': idx}
+                js = json.dumps(js)
+                resp = Response(js, status=404, mimetype='application/json')
+                return resp
+        
+        print("retrieved values: state, lighttime, dir, color", ints.curState,
+               ints.curActiveLightTimeLeft, ints.curActiveLightSet, ints.lightSet[ints.curActiveLightSet]['activeLight'])
+        # make sure it is still running
+        if (ints.curState != 'Running'):
+            del GlobalTimerThreads[idx]
+            return 1  # say we're done
+        
+        # check the current value of the remaining light time
+        if (ints.curActiveLightTimeLeft == 1): 
+            # time ran out. switch light
+            Intersection.switchLight(ints)
+        else:
+            # decrement and write back
+            ints.curActiveLightTimeLeft -= 1
+            write_to_redis(ints)
+        
+    @staticmethod
+    def switchLight(ints):
+        if ints.lightSet[ints.curActiveLightSet]['activeLight'] == 'Green': # go from green to Yellow
+            ints.lightSet[ints.curActiveLightSet]['activeLight'] =  'Yellow'
+            ints.curActiveLightTimeLeft = Intersection.YELLOWTIME
+        else:  # color must be yellow, because red timers don't run
+            # cycle direction to next one, change this direction to red, next to green, reset timer
+            ints.lightSet[ints.curActiveLightSet]['activeLight'] = 'Red'
+            ints.lightSet[ints.curActiveLightSet]['isActive'] = False
+            ints.curActiveLightSet = Intersection.DirectionNext[ints.curActiveLightSet]
+            ints.lightSet[ints.curActiveLightSet]['activeLight'] = 'Green'
+            ints.lightSet[ints.curActiveLightSet]['isActive'] = True
+            ints.curActiveLightTimeLeft = Intersection.GREENTIME
+        write_to_redis(ints)  # for now, just assume it works
+        return 0
+            
+        '''
         while True:  # loop forever - until thread is shut down
             for self.curActive in ['North', 'South', 'East', 'West']:
                 print("Changing direction to: ",self.curActive)
@@ -63,6 +198,7 @@ class Intersection:
                     if light != 'Red':
                         time.sleep(sleepTime)
                     # there is no else. this direction is done.
+        '''
     
     def getStatus(self):
             return "For signal at intersection ",self," Direction is ",self.curActive," Light is ",self.lightSet[self.curActive]['activeLight']
